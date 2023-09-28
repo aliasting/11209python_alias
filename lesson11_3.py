3d09152af180a0c4cedb235b3dc83fa43ee232ce
aliasting <alias0305@gmail.com>
2023-09-28 12:00:17 +0800

    modify

--- lesson11_3.py
+++ lesson11_3.py
@@ -4,6 +4,10 @@ class Person:
         self.weight = weight
         self.height = height
 
+    #method
+    def bmi(self) -> float:
+        return round(self.weight / (self.height / 100) ** 2,ndigits=2)
+
     def __str__(self) -> str:
         return f"name={self.name}\nweight={self.weight}\nheight={self.height}"
     
@@ -11,5 +15,7 @@ class Person:
 if __name__ == '__main__':
     p1 = Person("robert",78,183)
     print(p1)
+    print(p1.bmi())
+
     
     
