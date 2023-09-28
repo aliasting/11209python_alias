1829133b68f7a332407b2446b4c43ae25ea00945
aliasting <alias0305@gmail.com>
2023-09-28 16:01:47 +0800

    modify

---
+++ lesson12_2.py
@@ -0,0 +1,33 @@
+import random
+
+class Player:
+    def __init__(self):
+        pass
+
+    def __play() -> int:
+        pass
+
+    @property
+    def value(self) -> int:
+        #呼叫self.__play()
+        pass
+
+    def __repr__(self) -> str:
+        descript = ""
+        descript += "徐國堂\n"
+        descript += "骰子1=5\n"
+        descript += "骰子2=3\n"
+        descript += "骰子3=5\n"
+        descript += "骰子4=5\n"
+        dsecript += "得分=15分"
+        return descript
+    
+if __name__ == "__main__":
+    p1 = Player("robert")
+    print(p1.value)
+    print(p1)
+
+    p2 = Player("robert")
+    print(p2.value)
+    print(p2)
+
