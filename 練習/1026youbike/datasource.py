d321d722f116567ad5115c6e37f55761b4015437
aliasting <alias0305@gmail.com>
2023-10-26 10:31:42 +0800

    modify

--- 練習/1026youbike/datasource.py
+++ 練習/1026youbike/datasource.py
@@ -1,19 +1,19 @@
-import requests #讓人一進來就看到指令
+import requests  # 讓人一進來就看到指令
+
 
 def download_youbike_data():
-    '''
+    """
     下載台北市youbike資料2.0
     https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json
-    '''
-    #import requests 這個function會只用到這裡,所以還是寫在最上面
+    """
+    # import requests 這個function會只用到這裡,所以還是寫在最上面
 
-    youbike_url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+    youbike_url = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
 
     response = requests.get(youbike_url)
     if requests.status_codes == 200:
-       # print("下載成功")改寫
-       raise Exception("下載失敗")
+        # print("下載成功")改寫
+        raise Exception("下載失敗")
     else:
         # print("下載失敗")改寫
         raise Exception("下載失敗")
-        
